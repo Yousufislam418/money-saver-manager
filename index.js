b3efa8c368b624a8ec1c6db3ce78bac5ca68551e
Yousufislam418 <yousufislamaps@gmail.com>
2026-08-21 16:34:18 +0300

    cors add

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.post("/users", async (req,res)=> {
    if(existingUser){
     return res.status(400).json({error: 'User already create'});
    }else{
-    const users = new User({userdatas});
+    const users = new User(userdatas);
     const result = await users.save();  
     res.status(201).json(result);
    }
