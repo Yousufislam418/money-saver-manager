7282a219fec1388eb7b5435adae901ae53f23f0f
Yousufislam418 <yousufislamaps@gmail.com>
2026-09-05 19:44:15 +0300

    Autometic txn data add Update

--- models/cards.js
+++ models/cards.js
@@ -7,17 +7,17 @@ const cardsSchema = new mongoose.Schema({
  date: {type: Number, default: Date.now},
  expire: {type: Date, expires: 2592000},
  status: {type: String, default: "available"},
- txn: {type: Number, default: 0}
+ txn: {type: Number}
 },{
     versionKey: false,
     collection: "cards"
 }
 );
 // Txn id create + 1
-// cardsSchema.pre("save", async function(next) {
-//  if (!this.isNew) return next();
-//  const lastTxn = await this.constructor.findOne().sort({ txn: -1 });
-//   this.txn = lastTxn ? lastTxn.txn + 1 : 1;
-//   next();
-// });
+cardsSchema.pre("save", async function(next) {
+ if (!this.isNew) return next();
+ const lastTxn = await this.constructor.findOne().sort({ txn: -1 });
+  this.txn = lastTxn ? lastTxn.txn + 1 : 1;
+  next();
+});
 module.exports = mongoose.model("Cards", cardsSchema);
