49299f933799ba9a79730d096fe1dc974af33ff4
Yousufislam418 <yousufislamaps@gmail.com>
2026-09-05 20:26:14 +0300

    fixed update

--- models/cards.js
+++ models/cards.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
 
 const cardsSchema = new mongoose.Schema({
- cardbrandname: {type: String},
- cardnumber: {type: String},
- amount: {type: String},
- date: {type: Number, default: Date.now},
- expire: {type: Date, expires: 2592000},
- status: {type: String, default: "available"},
- txn: {type: Number}
+ cardbrandname: { type: String },
+ cardnumber: { type: String, unique: true },
+ amount: { type: String },
+ date: { type: Number, default: Date.now },
+ expire: { type: Date, expires: 2592000 },
+ status: { type: String, default: "available" },
+ txn: { type: Number, unique: true }
 },{
     versionKey: false,
     collection: "cards"
--- models/users.js
+++ models/users.js
@@ -1,15 +1,15 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    username: {type: String},
-    usernumber: {type: String, unique: true},
-    email: {type: String},
-    password: {type: String},
-    location: {type: String},
-    dates: {type: String},
-    status: {type: String},
-    balance: {type: String},
-    pin: {type: String}
+    username: { type: String },
+    usernumber: { type: String, unique: true },
+    email: { type: String },
+    password: { type: String },
+    location: { type: String },
+    dates: { type: String },
+    status: { type: String },
+    balance: { type: String },
+    pin: { type: String }
 },{
     versionKey: false,
     collection: 'users'
